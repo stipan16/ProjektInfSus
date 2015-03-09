2b2c3b13252f2f974294a19fc8bd212f48324a4e
stipan16 <slausi00@fesb.hr>
2015-03-09 10:34:39 +0100

    dodana klasa

---
+++ ProjektInfSus/ClasaPIS.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjektInfSus
+{
+    class ClasaPIS
+    {
+        public void ClasaPIS(){
+        }
+    }
+}
