e85b106b2841c785eac291e539afe0a6561b4e69
stipan16 <slausi00@fesb.hr>
2015-03-09 10:08:02 +0100

    init Commit

---
+++ ProjektInfSus/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjektInfSus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("09.03.2015");
+            Console.ReadLine();
+        }
+    }
+}
